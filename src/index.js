0920ee45b204a5270244d01db3dbb00e9f44d11d
khacker23 <khacker23@gmail.com>
2019-04-14 21:22:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//tutorial from https://reactjs.org/tutorial/tutorial.html
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,147 +5,94 @@ import './index.css';
 function Square(props) {
   return (
     <button className="square" onClick=
-    {props.onClick}>
-    {props.value}
+    {this.props.onClick}>
+    {this.props.squares}
+    {this.props.value}
     </button>
   );
 }
 
-class Board extends React.Component {
+class Grid extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isIconVisible : "",
+      squares: Array(30).fill(null)
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.displayIcon = this.displayIcon.bind(this);
+    this.renderSquare = this.renderSquare.bind(this);
+  }
+
   renderSquare(i) {
-    return (
+    return React.Children.map(this.props.squares, Square => {
+      return React.cloneElement(Square, {
+        value : this.props.squares[i],
+        onClick : this.displayIcon()
+      });
+    });
+    /* return (
     <Square 
       value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)} />
-    );
+      onClick={() => this.displayIcon()}
+       />
+    ); */
   }
+  
+  displayIcon() {
+    this.setState(this.isIconVisible ? <img src="https://firebasestorage.googleapis.com/v0/b/project-12e14.appspot.com/o/images%2Ffileicon2.png?alt=media&token=f4726298-d91d-4d4f-905d-1933002993e6" alt="File Icon" height="98" width="98" /> : "");
+    }
+
+    handleClick() {
+      this.setState(state => ({
+        isIconVisible: !state.isIconVisible
+      }));
+    }
 
   render() {
     
     return (
       <div>
-        <div className="board-row">
+        <div className="grid">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+        <div className="grid">
+          {this.renderSquare(10)}
+          {this.renderSquare(12)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+        </div>
+        <div className="grid">
+          {this.renderSquare(20)}
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
+          {this.renderSquare(25)}
+          {this.renderSquare(26)}
+          {this.renderSquare(27)}
+          {this.renderSquare(28)}
+          {this.renderSquare(29)}
         </div>
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    history: [{
-      squares: Array(9).fill(null),
-    }],
-    stepNumber: 0,
-    xIsNext: true,
-  };
-}
-
-handleClick(i) {
-  const history = this.state.history.slice(0, this.state.stepNumber + 1);
-  const current = history[history.length - 1];
-  const squares = current.squares.slice();
-  if (calculateWinner(squares) || squares[i]) {
-    return;
-  }
-  squares[i] = this.state.xIsNext ? 'X' : 'O';
-  this.setState({
-    history: history.concat([{
-      squares: squares,
-    }]),
-    stepNumber: history.length,
-    xIsNext: !this.state.xIsNext,
-  });
-}
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = 
-      calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-          <li key={move}>
-          <button onClick={() =>
-          this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' +
-      (this.state.xIsNext ? 'X' : 'O');
-    }
       
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-            />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
+    )
   }
 }
 
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+ReactDOM.render(<Grid />, document.getElementById("root"));
